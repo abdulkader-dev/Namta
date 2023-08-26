f4d6f1a3c32acecbc0bc2892e757eb5a1d567f32
abdulkader-dev <143296494+abdulkader-dev@users.noreply.github.com>
2023-08-27 05:29:56 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+for (var namta = 1; namta <= 5; namta++) {
+    document.write("<br>");
+    for (var i = 1; i <= 10; i++) {
+      document.write(namta + " X " + i + " = " + namta * i +"<br>");
+  }
+  
+  }
+  
+  
+  
+  
